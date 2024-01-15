36fc218287a04a7af6314fe1d5be2626d93780dc
FreddyPinto <freddypinto@outlook.com>
2024-01-15 08:30:01 -0500

    ✨ Add 'Recomendaciones' page section

---
+++ app/pages/2_🍴_Recomendaciones.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from utils import logo
+
+st.set_page_config(
+    page_title="Recomendaciones",
+    page_icon="🍴",
+    menu_items={
+    "Get help": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/wiki",
+    "Report a bug": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/issues/new",
+    "About": """
+        ## Yoogle
+        
+        **GitHub**: https://github.com/Insights-Labs-Consultant-Agency
+    """
+    }
+)
+
+logo()
+
+st.write("# Recomendaciones 🍴")
