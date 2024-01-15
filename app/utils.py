0b00f9e327e135f138e2a875aed3a8d8bbde47ae
FreddyPinto <freddypinto@outlook.com>
2024-01-15 08:35:46 -0500

    🛠 Add 'utils.py' file

---
+++ app/utils.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from streamlit_extras.app_logo import add_logo
+from streamlit_extras.switch_page_button import switch_page
+
+# Logo de la página
+def logo():
+    add_logo("assets/yoogle-icon.png", height=300)
+
+# Boton para cambiar de página 
+def switch_page():
+    asistente = st.button("Asistente")
+    recetas = st.button("Recetas")
+    if asistente:
+        switch_page("Asistente")
+    if recetas:
+        switch_page("Recetas")
