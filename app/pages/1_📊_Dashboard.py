16c9c9ccfd6df0a7ad8a0dbb74c19c03ea25968d
FreddyPinto <freddypinto@outlook.com>
2024-01-16 13:29:20 -0500

    📈 Add QuickSight integration

--- app/pages/1_📊_Dashboard.py
+++ app/pages/1_📊_Dashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from utils import logo
 
 st.set_page_config(
@@ -18,3 +19,17 @@ st.set_page_config(
 logo()
 
 st.title("📊 Dashboard ")
+
+
+# Código HTML del dashboard de QuickSight
+html_code = """
+    <iframe
+        width="960"
+        height="720"
+        src="https://us-east-2.quicksight.aws.amazon.com/sn/embed/share/accounts/018079189591/dashboards/1077495c-a58e-4910-a34e-909e4780fb9d?directory_alias=insights-labs-quicksight">
+    </iframe>
+"""
+
+# Insertar el dashboard de QuickSight en la aplicación de Streamlit
+components.html(html_code, height=720)
+
