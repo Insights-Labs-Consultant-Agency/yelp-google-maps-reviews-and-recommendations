ca3de5e80b199cf8a9b630920c34ce004e9d6db2
FreddyPinto <freddypinto@outlook.com>
2024-01-16 21:26:20 -0500

    💄Add adaptive iframe for QuickSight dashboard

--- app/pages/1_📊_Dashboard.py
+++ app/pages/1_📊_Dashboard.py
@@ -5,6 +5,8 @@ from utils import logo
 st.set_page_config(
     page_title="Dashboard",
     page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="collapsed",
     menu_items={
     "Get help": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/wiki",
     "Report a bug": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/issues/new",
@@ -23,13 +25,26 @@ st.title("📊 Dashboard ")
 
 # Código HTML del dashboard de QuickSight
 html_code = """
-    <iframe
-        width="960"
-        height="720"
-        src="https://us-east-2.quicksight.aws.amazon.com/sn/embed/share/accounts/018079189591/dashboards/1077495c-a58e-4910-a34e-909e4780fb9d?directory_alias=insights-labs-quicksight">
-    </iframe>
+    <div style="
+        position: relative;
+        padding-bottom: 56.25%; /* Proporción 16:9 */
+        height: 0;
+        overflow: hidden;
+    ">
+        <iframe
+            src="https://us-east-2.quicksight.aws.amazon.com/sn/embed/share/accounts/018079189591/dashboards/1077495c-a58e-4910-a34e-909e4780fb9d?directory_alias=insights-labs-quicksight"
+            style="
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                border: none;
+            ">
+        </iframe>
+    </div>
 """
 
-# Insertar el dashboard de QuickSight en la aplicación de Streamlit
+# Inserta el dashboard de QuickSight en la aplicación de Streamlit
 components.html(html_code, height=720)
 
