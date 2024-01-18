ff7fb0e74e820ecee03ae3769386363ae80e40b3
FreddyPinto <freddypinto@outlook.com>
2024-01-18 01:35:34 -0500

    👔 Update business logic

--- app/pages/1_📊_Dashboard.py
+++ app/pages/1_📊_Dashboard.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import streamlit.components.v1 as components
-from utils import logo
+from utils import logo, quicksight_dashboard
 
 st.set_page_config(
     page_title="Dashboard",
@@ -22,30 +21,5 @@ logo()
 
 st.title("📊 Dashboard ")
 
-
-# Código HTML del dashboard de QuickSight
-html_code = """
-    <div style="
-        position: relative;
-        padding-bottom: 56.25%; /* Proporción 16:9 */
-        height: 0;
-        overflow: hidden;
-    ">
-        <iframe
-            src="https://us-east-2.quicksight.aws.amazon.com/sn/embed/share/accounts/018079189591/dashboards/85c5c680-f2d0-4a84-b6d0-7d8f24414031?directory_alias=insights-labs-quicksight">
-            style="
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                border: none;
-            ">
-        </iframe>
-    </div>
-"""
-
-
-# Inserta el dashboard de QuickSight en la aplicación de Streamlit
-components.html(html_code, height=720)
+quicksight_dashboard()
 
