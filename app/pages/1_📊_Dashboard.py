6e1034669b5504f8bbb9e4ac163abe31707a61fd
FreddyPinto <freddypinto@outlook.com>
2024-01-17 21:03:40 -0500

    📈 Add embedded Power BI dashboard

--- app/pages/1_📊_Dashboard.py
+++ app/pages/1_📊_Dashboard.py
@@ -1,13 +1,16 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from utils import logo
 
 st.set_page_config(
     page_title="Dashboard",
     page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="collapsed",
     menu_items={
-    "Get help": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/wiki",
-    "Report a bug": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/issues/new",
-    "About": """
+        "Get help": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/wiki",
+        "Report a bug": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/issues/new",
+        "About": """
         ## Yoogle
         
         **GitHub**: https://github.com/Insights-Labs-Consultant-Agency
@@ -18,3 +21,20 @@ st.set_page_config(
 logo()
 
 st.title("📊 Dashboard ")
+
+
+# Código HTML del dashboard de QuickSight
+html_code = """
+    <div style="
+        position: relative;
+        padding-bottom: 56.25%; /* Proporción 16:9 */
+        height: 0;
+        overflow: hidden;
+    ">
+        <iframe title="1.0-fp-dashboard" width="1220" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiMWM4ZDI1OTUtOGU1Ny00OTU3LTg5YTUtNmIzNzQ2YmQzODViIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9" frameborder="0" allowFullScreen="true">
+        </iframe>
+    </div>
+"""
+
+# Inserta el dashboard de QuickSight en la aplicación de Streamlit
+components.html(html_code, height=720)
