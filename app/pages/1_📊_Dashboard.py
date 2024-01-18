613585b11a56b0e88a6211817259b78d88d42f51
FreddyPinto <freddypinto@outlook.com>
2024-01-18 01:49:38 -0500

    👔 Update business logic

--- app/pages/1_📊_Dashboard.py
+++ app/pages/1_📊_Dashboard.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import streamlit.components.v1 as components
-from utils import logo
+from utils import logo, quicksight_dashboard
 
 st.set_page_config(
     page_title="Dashboard",
@@ -18,54 +17,10 @@ st.set_page_config(
     }
 )
 
+# Inserta el logo en el sidebar
 logo()
 
 st.title("📊 Dashboard ")
 
-# Código HTML del dashboard de QuickSight
-html_code = """
-<html>
-<head>
-    <style>
-        .iframe-container {
-            overflow: hidden;
-            padding-top: 56.25%; /* 16:9 */
-            position: relative;
-        }
-
-        .iframe-container iframe {
-            position: absolute;
-            top: 0;
-            left: 0;
-            border: 0;
-            width: 100%;
-            height: 100%;
-            background-color: transparent;
-        }
-    </style>
-</head>
-<body>
-    <div class="iframe-container">
-        <iframe src="https://us-east-2.quicksight.aws.amazon.com/sn/embed/share/accounts/018079189591/dashboards/85c5c680-f2d0-4a84-b6d0-7d8f24414031?directory_alias=insights-labs-quicksight" frameborder="0" allowfullscreen></iframe>
-    </div>
-</body>
-</html>
-"""
-
-# width="1220"
-# height="720"
-# # Código HTML del dashboard de PowerBI
-# html_code = """
-#     <div style="
-#         position: relative;
-#         padding-bottom: 56.25%; /* Proporción 16:9 */
-#         height: 0;
-#         overflow: hidden;
-#     ">
-#         <iframe title="1.0-fp-dashboard" width="1220" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiMWM4ZDI1OTUtOGU1Ny00OTU3LTg5YTUtNmIzNzQ2YmQzODViIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9" frameborder="0" allowFullScreen="true">
-#         </iframe>
-#     </div>
-# """
-
-# Inserta el dashboard de QuickSight en la aplicación de Streamlit
-components.html(html_code, height=720)
+# Inserta el dashboard de QuickSight
+quicksight_dashboard()
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 from streamlit_extras.switch_page_button import switch_page
+import streamlit.components.v1 as components
 
 # Logo de la página
 def logo():
@@ -14,3 +15,57 @@ def switch_page():
         switch_page("Asistente")
     if recetas:
         switch_page("Recetas")
+
+
+def quicksight_dashboard():
+    # Código HTML del dashboard de QuickSight
+    html_code = """
+    <html>
+    <head>
+        <style>
+            .iframe-container {
+                overflow: hidden;
+                padding-top: 56.25%; /* 16:9 */
+                position: relative;
+            }
+
+            .iframe-container iframe {
+                position: absolute;
+                top: 0;
+                left: 0;
+                border: 0;
+                width: 100%;
+                height: 100%;
+                background-color: transparent;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="iframe-container">
+            <iframe src="https://us-east-2.quicksight.aws.amazon.com/sn/embed/share/accounts/018079189591/dashboards/85c5c680-f2d0-4a84-b6d0-7d8f24414031?directory_alias=insights-labs-quicksight" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </body>
+    </html>
+    """
+
+    # Inserta el dashboard de QuickSight en la aplicación de Streamlit
+    components.html(html_code, height=720)
+
+
+# def powerbi_dashboard():
+    # width="1220"
+    # height="720"
+    # # Código HTML del dashboard de PowerBI
+    # html_code = """
+    #     <div style="
+    #         position: relative;
+    #         padding-bottom: 56.25%; /* Proporción 16:9 */
+    #         height: 0;
+    #         overflow: hidden;
+    #     ">
+    #         <iframe title="1.0-fp-dashboard" width="1220" height="780" src="https://app.powerbi.com/view?r=eyJrIjoiMWM4ZDI1OTUtOGU1Ny00OTU3LTg5YTUtNmIzNzQ2YmQzODViIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9" frameborder="0" allowFullScreen="true">
+    #         </iframe>
+    #     </div>
+    # """
+    # Inserta el dashboard de QuickSight en la aplicación de Streamlit
+    # components.html(html_code, height=720)
