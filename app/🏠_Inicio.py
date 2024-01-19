56c90b67c6dbd4df09ce66022ba8c84b22e9ee1f
FreddyPinto <freddypinto@outlook.com>
2024-01-19 09:07:31 -0500

    ✨ Add login authenticator

--- app/🏠_Inicio.py
+++ app/🏠_Inicio.py
@@ -1,9 +1,17 @@
 import streamlit as st
 from utils import logo, switch_page
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+# Configuración de la página
 st.set_page_config(
     page_title="Yoogle",
-    page_icon="app/assets/yoogle-favicon-32x32.png",
+    page_icon="assets/yoogle-favicon-32x32.png",
     menu_items={
     "Get help": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/wiki",
     "Report a bug": "https://github.com/Insights-Labs-Consultant-Agency/yelp-google-maps-reviews-and-recommendations/issues/new",
@@ -18,39 +26,56 @@ st.set_page_config(
 # Agregar el logotipo a la barra lateral
 logo()
 
-st.title("Bienvenido a Yoogle! 👋")
-st.markdown('_**Descubre, elige, y disfruta la comida vegetariana**_')
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-st.divider()
+name, authentication_status, username = authenticator.login('Login', 'main')
 
-#Use Cases
-with st.container():
-    col1,col2=st.columns(2)
-    with col1:
-        st.subheader('¿Qué puedes hacer?')
-        st.markdown(
-            """
-            Yoogle te permite acceder a una gran variedad de funcionalidades que harán tu vida más fácil y divertida. Con Yoogle, puedes:
-            - _Explorar el mercado de comida vegetariana en Estados Unidos_
-            - _Encontrar el restaurante ideal para ti_
-            - _Conversar con un asistente virtual inteligente_
-            - _Generar recetas saludables y deliciosas_
-            """
-            )
-    with col2:
-        st.subheader("¿Por qué usar Yoogle?")
-        st.markdown(
-            """
-            Yoogle no solo te ofrece información, sino también beneficios. Al usar Yoogle, podrás:
-            - _Descubrir nuevos lugares_
-            - _Ahorrar tiempo y dinero_
-            - _Disfrutar de una alimentación sana y variada_
-            """
-            )
 
-st.divider()
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.title(f"Bienvenido a Yoogle, *{st.session_state['name']}*! 👋")
+    st.markdown('_**Descubre, elige, y disfruta la comida vegetariana**_')
 
-st.subheader("¿Listo para explorar Yoogle?")
-st.markdown(" Elige una de las opciones de abajo y empieza tu aventura gastronómica.")
+    st.divider()
 
-switch_page()
+    # Casos de usoc:\Users\Freddy\Downloads\config.yaml
+    with st.container():
+        col1,col2=st.columns(2)
+        with col1:
+            st.subheader('¿Qué puedes hacer?')
+            st.markdown(
+                """
+                Yoogle te permite acceder a una gran variedad de funcionalidades que harán tu vida más fácil y divertida. Con Yoogle, puedes:
+                - _Explorar el mercado de comida vegetariana en Estados Unidos_
+                - _Encontrar el restaurante ideal para ti_
+                - _Conversar con un asistente virtual inteligente_
+                - _Generar recetas saludables y deliciosas_
+                """
+                )
+        with col2:
+            st.subheader("¿Por qué usar Yoogle?")
+            st.markdown(
+                """
+                Yoogle no solo te ofrece información, sino también beneficios. Al usar Yoogle, podrás:
+                - _Descubrir nuevos lugares_
+                - _Ahorrar tiempo y dinero_
+                - _Disfrutar de una alimentación sana y variada_
+                """
+                )
+
+    st.divider()
+
+    st.subheader("¿Listo para explorar Yoogle?")
+    st.markdown(" Elige una de las opciones de abajo y empieza tu aventura gastronómica.")
+
+    switch_page()
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
